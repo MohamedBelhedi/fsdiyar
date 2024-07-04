bb659f0e3c7c38ba96f6110c4971cd76d42c937a
Mohamed Belhedi <mohamed.belhedi@yadbo.com>
2024-07-04 19:01:18 +0200

    Update 2.1 prüfungen der Aktuellen Woche FixUp DateFormat in Setting

--- fahrschulemanager_fullstack/admin.py
+++ fahrschulemanager_fullstack/admin.py
@@ -1,37 +1,5 @@
-# from django.contrib import admin
-# from import_export import resources
-# from import_export.admin import ImportExportModelAdmin
-# from .models import Events, Prüflinge, AktuellePrüfungListe
-#
-# class PrüflingeResource(resources.ModelResource):
-#     class Meta:
-#         model = Prüflinge
-#         fields = ('fl', 'name', 'bezahlt', 'prüfungsdatum')
-#         exclude = ('id',)
-#
-# class PrüfungResource(resources.ModelResource):
-#     class Meta:
-#         model = AktuellePrüfungListe
-#         fields = ('fl', 'name', 'prüfungsdatum', 'uhrzeit')
-#         exclude = ('id',)
-#
-# @admin.register(Prüflinge)
-# class PrüflingeAdmin(ImportExportModelAdmin):
-#     resource_class = PrüflingeResource
-#     list_display = ('fl', 'name', 'bezahlt', 'prüfungsdatum')
-#     exclude = ('id',)
-#
-# @admin.register(AktuellePrüfungListe)
-# class AktuellePrüfungListeAdmin(ImportExportModelAdmin):
-#     resource_classes = [PrüfungResource]
-#     list_display = ('fl', 'name', 'prüfungsdatum', 'uhrzeit')
-#     exclude = ('id',)
-#
-# admin.site.register(Events)
-
 from django.contrib import admin
-from import_export import resources, fields
-from import_export.widgets import ForeignKeyWidget
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import Events, Prüflinge, AktuellePrüfungListe
 
@@ -47,6 +15,12 @@ class PrüfungResource(resources.ModelResource):
         exclude = ('id',)  # Exclude the ID field
         import_id_fields = ('fl',)  # Use 'fl' as the unique identifier for import
 
+class EventsResource(resources.ModelResource):
+    class Meta:
+        model = Events
+        exclude = ('id',)  # Exclude the ID field
+        import_id_fields = ('date',)  # Use a unique identifier field for import
+
 @admin.register(Prüflinge)
 class PrüflingeAdmin(ImportExportModelAdmin):
     resource_class = PrüflingeResource
@@ -57,5 +31,8 @@ class AktuellePrüfungListeAdmin(ImportExportModelAdmin):
     resource_classes = [PrüfungResource]  # Ensure this is a list
     list_display = ('fl', 'name', 'prüfungsdatum', 'uhrzeit')
 
-admin.site.register(Events)
+@admin.register(Events)
+class EventsAdmin(ImportExportModelAdmin):
+    resource_class = EventsResource
+    list_display = ('date', 'text_event', 'uhrzeit')
 
