1ef3ec94d915af30994d6ddf35e56d731dd4d987
Mohamed Belhedi <mohamed.belhedi@yadbo.com>
2024-07-06 10:27:19 +0200

    Update 2.2 Styling Burger Menü und löschen der Events Autom.

--- fahrschulmanager/settings.py
+++ fahrschulmanager/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-45++%_2#4k!%15afe(p*6wa)-jztilna6wm7ec12q(z%3dh$(0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['https://fsdiyar.onrender.com/']
