20f89e85bb8780ad6936780b2bcfd8388fe06354
Mohamed Belhedi <mohamed.belhedi@yadbo.com>
2025-01-12 15:32:42 +0100

    Update 2.9 Whatsapp Support für Fahrlehrer/Büro muss noch bearbeitet werden entweder kommt es unter den prüfungen oder wo anders noch ein UI für die Praxis damit man nicht immer über admin anmelden muss (ist eine Idee)

--- fahrschulemanager_fullstack/views.py
+++ fahrschulemanager_fullstack/views.py
@@ -158,7 +158,8 @@ def theories(request):
     if request.method == "POST" and "whatsapp" in request.POST:
         for items in pruefungTuev:
             print(items.date)
-            pywhatkit.sendwhatmsg_to_group_instantly("KS1GVUvBxDKFvbm686Olvx", f"Bitte einmal anmelden ueber die App (https://fsdiyar.onrender.com/) fuer den: {items.date.strftime("%d.%m.%Y")} {items.text_event} Pruefungen")
+            #pywhatkit.sendwhatmsg_to_group_instantly("KS1GVUvBxDKFvbm686Olvx", f"Bitte einmal anmelden ueber die App (https://fsdiyar.onrender.com/) fuer den: {items.date.strftime("%d.%m.%Y")} {items.text_event} Pruefungen")
+            pywhatkit.sendwhatmsg_to_group_instantly("KS1GVUvBxDKFvbm686Olvx", items.date.strftime("%d.%m.%Y") + items.text_event )
 
     context = {
         "pruefungTheorie": pruefungTheorie,
